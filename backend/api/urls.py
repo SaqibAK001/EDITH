1af5b48e604987d5ffdb6db6d2ed3810c31bce98
SaqibAK001 <saqibak045@gmail.com>
2026-06-05 10:58:37 +0530

    Updated rendering

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,6 +1,18 @@
 from django.urls import path
 from .views import ask_document
 
+from django.contrib import admin
+from django.urls import path, include
+from django.http import JsonResponse
+
+def home(request):
+    return JsonResponse({
+        "status": "running",
+        "message": "EDITH Backend API"
+    })
+
 urlpatterns = [
-    path("ask/", ask_document),
+    path("", home),
+    path("api/", include("api.urls")),
+    path("admin/", admin.site.urls),
 ]
